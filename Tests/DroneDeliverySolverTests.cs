a81b6727a74d0c41d5462bebf0cffea4171744b1
Marco <marco@logstore.com.br>
2023-05-16 20:39:51 -0300

    adde more tests

--- Tests/DroneDeliverySolverTests.cs
+++ Tests/DroneDeliverySolverTests.cs
@@ -1,3 +1,4 @@
+using Src.Helpers;
 using Src.Models;
 using Src.Services;
 
@@ -74,6 +75,116 @@ public class DroneDeliverySolverTests
                              "\n";
 
         Assert.Equal(expectedOutput, NormalizeLineEndings(output));
+    }
+
+    [Fact]
+    public void Solve_ValidSolution()
+    {
+        // Arrange
+        List<Drone> drones = new List<Drone>
+    {
+        new Drone("Drone 1", 10),
+        new Drone("Drone 2", 15)
+    };
+
+        List<Location> locations = new List<Location>
+    {
+        new Location("Location 1", 5),
+        new Location("Location 2", 8),
+        new Location("Location 3", 12)
+    };
+
+        // Act
+        DroneDeliverySolver solver = new DroneDeliverySolver();
+        solver.Initialize(drones, locations);
+        solver.Solve();
+
+        // Assert
+        // Add assertions to validate the solution
+        // For example, check if each drone's trips respect the weight limits
+        foreach (Drone drone in drones)
+        {
+            foreach (List<Location> trip in drone.Trips)
+            {
+                int totalWeight = trip.Sum(location => location.PackageWeight);
+                Assert.True(totalWeight <= drone.MaximumWeight);
+            }
+        }
+    }
+
+    [Fact]
+    public void Print_ReturnsCorrectOutputString()
+    {
+        // Arrange
+        List<Drone> drones = new List<Drone>
+    {
+        new Drone("Drone 1", 10),
+        new Drone("Drone 2", 15)
+    };
+
+        List<Location> locations = new List<Location>
+    {
+        new Location("Location 1", 5),
+        new Location("Location 2", 8),
+        new Location("Location 3", 12)
+    };
+
+        DroneDeliverySolver solver = new DroneDeliverySolver();
+        solver.Initialize(drones, locations);
+        // Call the Solve method to populate the trips
+
+        // Act
+        string output = solver.Print();
+
+        // Assert
+        string expectedOutput = "[Drone 1]\nTrip #1\nLocation 1\n\n[Drone 2]\nTrip #1\nLocation 2\n";
+        Assert.Equal(expectedOutput, output);
+    }
+
+    [Fact]
+    public void WriteOutputFile_WritesCorrectOutputFile()
+    {
+        // Arrange
+        string outputFile = "output.txt";
+        string outputContent = "Test output content";
+
+        // Act
+        OutputWriter.WriteOutputFile(outputFile, outputContent);
+
+        // Assert
+        string actualContent = File.ReadAllText(outputFile);
+        Assert.Equal(outputContent, actualContent);
+    }
+
+    [Fact]
+    public void ReadInputFile_ReturnsCorrectData()
+    {
+        // Arrange
+        string inputFile = "input.txt";
+        string[] inputLines = new string[]
+        {
+        "Drone 1, 10",
+        "Location 1, 5",
+        "Location 2, 8"
+        };
+        // Arrange
+        File.WriteAllLines(inputFile, inputLines);
+
+        // Act
+        var (drones, locations) = InputReader.ReadInputFile(inputFile);
+
+        // Assert
+        Assert.Equal(2, drones.Count);
+        Assert.Equal(2, locations.Count);
+
+        Assert.Equal("Drone 1", drones[0].Name);
+        Assert.Equal(10, drones[0].MaximumWeight);
+
+        Assert.Equal("Location 1", locations[0].Name);
+        Assert.Equal(5, locations[0].PackageWeight);
+
+        Assert.Equal("Location 2", locations[1].Name);
+        Assert.Equal(8, locations[1].PackageWeight);
     }
     private static string NormalizeLineEndings(string text)
     {
